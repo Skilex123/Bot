cd2fd9350b6f62f99152ea910c42f04d949e7991
Mykhailo  Horbachenko <mykhailo.horbachenko@smartexe.com>
2023-01-30 15:17:01 +0200

    Added execution of query to create table

--- database/queries.py
+++ database/queries.py
@@ -1,11 +1,16 @@
+from connection import Conn
 
-CREATE_FLOWERS_TABLE = """CREATE TABLE flowers(
-
-    ID int NOT NULL PRIMARY KEY,
-    price DECIMAL(3,2) NOT NULL,
-    amount  int NOT NULL,
-    name varchar(250) NOT NULL UNIQUE,
-    color varchar(250) NOT NULL
-    
-);
+CREATE_FLOWERS_TABLE = """
+    CREATE TABLE flowers(
+        ID int NOT NULL PRIMARY KEY,
+        price DECIMAL(3,2) NOT NULL,
+        amount  int NOT NULL,
+        name varchar(250) NOT NULL UNIQUE,
+        color varchar(250) NOT NULL
+    );
 """
+
+
+if __name__ == "__main__":
+    db_instance = Conn()
+    db_instance.create_table(CREATE_FLOWERS_TABLE)
