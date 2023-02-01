b7c286e5a503e835510d4778446488c886b6f458
Skilex123 <skilex57@gmail.com>
2023-02-01 10:27:28 +0200

    adding a generator

--- database/predefined.py
+++ database/predefined.py
@@ -6,8 +6,17 @@ def read_json(file_name):
         return content
 
 
+def iterate_list(iterable_sequence):
+    try:
+        yield from iterable_sequence
+    except Exception:
+        print('Должен быть список')
+        raise
+
+
 if __name__ == "__main__":
     FILE_NAME = 'flowers.json'
-    result = read_json(file_name=FILE_NAME)
-    print (result)
-    
+    list_of_flowers = read_json(file_name=FILE_NAME)
+    flowers = iterate_list(list_of_flowers)
+    print(flowers)
+
